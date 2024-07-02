f675ed6da722425eca9df18fa96c5f18bb28262f
Willyam Mach <willyam.mach@gmail.com>
2024-07-02 14:59:04 -0300

    add absolute path

--- conexao.php
+++ conexao.php
@@ -1,7 +1,9 @@
 <?php
+$caminhoBanco =  __DIR__ . '/banco.sqlite';
 
 try {
-    $pdo = new PDO('sqlite:banco.sqlite');
+    $pdo = new PDO('sqlite:'.$caminhoBanco);
+
 } catch(PDOException $e ) {
     echo "Erro ".$e->getMessage();
 }
